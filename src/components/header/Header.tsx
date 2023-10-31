ee41c918b0abc9333cf3ea7e42c67896126c5243
Maestershadow <Maestershadow@gmail.com>
2023-10-31 11:34:29 +0000

    mobile nav completed

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,20 +1,34 @@
 import './Header.css'
 import NavListing from '../common/navListing'
+import { useState } from 'react'
 
 export default function Header() {
+
+    const [navOpened, setNavOpened] = useState(false);
+
+
+
+
     return <header className="header">
         <div className='header-container'>
             <div className='header-head'>
                 <div>
                     <img src="images/logo.svg" alt="main logo" />
                 </div>
-                <nav className='nav-list'>
+                <nav className='nav-list' data-visible={`${navOpened}`}>
+                    <div className='mobile-nav-head'>
+                        <div>
+                            <img src="images/logo.svg" alt="main logo" />
+                        </div>
+                        <button style={{padding:0}} onClick={() => setNavOpened(!navOpened)}>
+                            <img src="images/icon-close.svg" alt="close" />
+                        </button>
+                    </div>
                     <NavListing />
                 </nav>
-                <div className="menu-btn" data-visible="false">
-                    <img src="images/icon-close.svg" alt="close" />
+                <button onClick={() => setNavOpened(!navOpened)} className="menu-btn" data-visible={`${navOpened}`}>
                     <img src="images/icon-hamburger.svg" alt="close" />
-                </div>
+                </button>
             </div>
 
             <div className='header-tail'>
